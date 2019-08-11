600ced3cd44f1ae667c839af8adeba1c1e9b5f41
Ekaterina Chachik <Ekaterina_Chachik@epam.com>
2019-08-12 00:10:19 +0300

    add comments

--- Java/src/main/java/Planes/MyPlane.java
+++ Java/src/main/java/Planes/MyPlane.java
@@ -1,9 +1,19 @@
 package Planes;
 
+
+
+/**нельзя называть класс MyPlane - не несет смысловой нагрузки. Самолет должен быть военный, пасажирский и.т.д
+ */
+
+
 public class MyPlane  extends Plane {
     public MyPlane(String model, int maxSpeed, int maxFlightDistance, int maxLoadCapacity) {
         super(model, maxSpeed, maxFlightDistance, maxLoadCapacity);
     }
+
+    /**
+     служеюные коментарии, не стоит комитить в git
+     */
 
     //TODO implement it later
 }
